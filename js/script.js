3401696ef95fcaba93d639bed6f0cd22470b17e1
Vonschell <vonschell02@gmail.com>
2024-12-13 19:31:19 +0900

    Added the event listener change to subTypeElement

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ var subDurationElement = document.querySelector("#months");
 var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e) {
+    subType = e.target.value;
+    console.log(subType);
+});
+
