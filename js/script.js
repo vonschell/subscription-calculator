e2f71bf0bf74e70ff3fdd24b0aebdb6b97df2cb4
Vonschell <vonschell02@gmail.com>
2024-12-13 19:23:14 +0900

    Created variables for subTypeElement, subDurationElement, result, subType and subDuration

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 // Your JS code here
-var 
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
